a5b43d85b691a5a2a43bdaaeef4c797813a8e5af
Milos <miskoo995@gmail.com>
2025-06-30 12:39:15 +0200

    test

--- routes/mealRoutes.js
+++ routes/mealRoutes.js
@@ -7,8 +7,8 @@ const {
 const { protect, authorize } = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/:id", protect, authorize("admin"), mealById);
-router.post("/byNutrients", protect, authorize("admin"), mealsByNutrients);
+// router.get("/:id", protect, authorize("admin"), mealById);
+// router.post("/byNutrients", protect, authorize("admin"), mealsByNutrients);
 router.post("/addMeal", protect, authorize("admin"), addMeal);
 
 module.exports = router;
