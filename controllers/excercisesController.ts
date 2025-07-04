0b332603574b1c08be2d31984e154d1fd0e1b291
Milos Marjanovic <miskoo995@gmail.com>
2025-07-05 01:05:28 +0200

    testing out

--- controllers/excercisesController.ts
+++ controllers/excercisesController.ts
@@ -2,130 +2,109 @@ import catchAsync from "../middleware/async";
 import AppError from "../utils/appError";
 import Excercise from "../models/Excercise";
 import { Request, Response, NextFunction } from "express";
-// import APIFeatures from "../utils/apiFeatures";
+import APIFeatures from "../utils/apiFeatures";
 // @desc    Gets All Excercises
 // @access  Private
 // @route   GET /api/v1/excercise/excercises
-export const getAllExcercises = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.user || !req.user.id) {
-      return next(new AppError("User not authenticated", 401));
-    }
-    const ownerId = req?.user?.id;
-    const excercises = await Excercise.find({ ownerId });
-
-    res.status(200).json({ success: true, data: excercises });
+export const getAllExcercises = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user || !req.user.id) {
+    return next(new AppError("User not authenticated", 401));
   }
-);
+  const ownerId = req?.user?.id;
+
+  const excercises = new APIFeatures(Excercise.find({ ownerId }), req.query).sort().paginate();
+
+  // const doc = await excercises.query;
+
+  res.status(200).json({ success: true, data: {} });
+});
 
 // @desc    Gets One Excercise by ID
 // @access  Private
 // @route   GET /api/v1/excercise/:excerciseId
-export const getExcerciseById = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.user || !req.user.id) {
-      return next(new AppError("User not authenticated", 401));
-    }
-    // const ownerId = req.user.id;
-
-    const { excerciseId } = req.params;
-    const excercise = await Excercise.findById(excerciseId);
-
-    res.status(200).json({ success: true, data: excercise });
+export const getExcerciseById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user || !req.user.id) {
+    return next(new AppError("User not authenticated", 401));
   }
-);
+  // const ownerId = req.user.id;
+
+  const { excerciseId } = req.params;
+  const excercise = await Excercise.findById(excerciseId);
+
+  res.status(200).json({ success: true, data: excercise });
+});
 
 // @desc    Add new Excercise
 // @access  Private
 // @route   POST /api/v1/excercise/addExcercise
-export const addExcercise = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { name, video, notes } = req.body;
+export const addExcercise = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { name, video, notes } = req.body;
 
-    if (!req.user || !req.user.id) {
-      return next(new AppError("User not authenticated", 401));
-    }
-    const ownerId = req.user.id;
-
-    if (!name || !video || !notes) {
-      return next(new AppError("All fields are required", 400));
-    }
-
-    const excercise = await Excercise.create({
-      name,
-      video,
-      notes,
-      ownerId,
-    });
-
-    res.status(200).json({
-      status: "success",
-      data: excercise,
-    });
+  if (!req.user || !req.user.id) {
+    return next(new AppError("User not authenticated", 401));
   }
-);
+  const ownerId = req.user.id;
+
+  if (!name || !video || !notes) {
+    return next(new AppError("All fields are required", 400));
+  }
+
+  const excercise = await Excercise.create({
+    name,
+    video,
+    notes,
+    ownerId,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: excercise,
+  });
+});
 
 // @desc    Update Excercise
 // @access  Private
 // @route   PUT /api/v1/excercise/updateExcercise
-export const updateExcercise = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
-    const { name, video, notes } = req.body;
+export const updateExcercise = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { name, video, notes } = req.body;
 
-    const ownerId = req?.user?.id;
+  const ownerId = req?.user?.id;
 
-    const updatedExcercise = await Excercise.findOneAndUpdate(
-      { id: id, ownerId },
-      { name, video, notes },
-      { new: true, runValidators: true }
-    );
+  const updatedExcercise = await Excercise.findOneAndUpdate({ id: id, ownerId }, { name, video, notes }, { new: true, runValidators: true });
 
-    if (!updatedExcercise) {
-      return next(
-        new AppError(
-          "Excercise not found or you are not authorized to update it.",
-          404
-        )
-      );
-    }
-    res.status(200).json({
-      status: "success",
-      data: updatedExcercise,
-    });
+  if (!updatedExcercise) {
+    return next(new AppError("Excercise not found or you are not authorized to update it.", 404));
   }
-);
+  res.status(200).json({
+    status: "success",
+    data: updatedExcercise,
+  });
+});
 
 // @desc    Delete Excercise
 // @access  Private
 // @route   DELETE /api/v1/excercise/deleteExcercise
 
-export const deleteExcercise = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
+export const deleteExcercise = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
 
-    if (!req.user || !req.user.id) {
-      return next(new AppError("User not authenticated", 401));
-    }
-    const ownerId = req.user.id;
-
-    const deleteExcercise = await Excercise.findOneAndDelete({
-      id,
-      ownerId,
-    });
-
-    if (!deleteExcercise) {
-      return next(
-        new AppError(
-          "Excercise not found or you are not authorized to update it.",
-          404
-        )
-      );
-    }
-
-    res.status(200).json({
-      status: "success",
-      data: {},
-    });
+  if (!req.user || !req.user.id) {
+    return next(new AppError("User not authenticated", 401));
   }
-);
+  const ownerId = req.user.id;
+
+  const deleteExcercise = await Excercise.findOneAndDelete({
+    id,
+    ownerId,
+  });
+
+  if (!deleteExcercise) {
+    return next(new AppError("Excercise not found or you are not authorized to update it.", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {},
+  });
+});
--- controllers/mealsController.ts
+++ controllers/mealsController.ts
@@ -8,160 +8,132 @@ import { Request, Response, NextFunction } from "express";
 // @access  Private
 // @route   POST /api/v1/meals/byNutrients
 
-export const mealsByNutrients = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { maxCarbs, minCarbs, maxProtein, minProtein } = req.body;
+export const mealsByNutrients = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { maxCarbs, minCarbs, maxProtein, minProtein } = req.body;
 
-    if (!minCarbs && !maxCarbs && !minProtein && !maxProtein) {
-      return next(
-        new AppError("You need to set at least one of the nutrients!", 400)
-      );
-    }
-
-    const response = await recepiesByNutrients({
-      maxCarbs,
-      minCarbs,
-      maxProtein,
-      minProtein,
-    });
-
-    res.status(201).json({
-      status: "success",
-      data: response,
-    });
+  if (!minCarbs && !maxCarbs && !minProtein && !maxProtein) {
+    return next(new AppError("You need to set at least one of the nutrients!", 400));
   }
-);
+
+  const response = await recepiesByNutrients({
+    maxCarbs,
+    minCarbs,
+    maxProtein,
+    minProtein,
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: response,
+  });
+});
 // @desc    Find meal detail by id from Spoonacular API
 // @access  Private
 // @route   GET /api/v1/meals/mealDetails/:id
-export const mealById = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
+export const mealById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
 
-    if (!id) {
-      return next(new AppError("Meal ID is required", 400));
-    }
-
-    const meal = await recepieById(id);
-    if (!meal) {
-      return next(new AppError("Meal not found", 404));
-    }
-
-    res.status(200).json({
-      status: "success",
-      data: meal,
-    });
+  if (!id) {
+    return next(new AppError("Meal ID is required", 400));
   }
-);
+
+  const meal = await recepieById(id);
+  if (!meal) {
+    return next(new AppError("Meal not found", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: meal,
+  });
+});
 
 // @desc    Get all saved meals
 // @access  Private
 // @route   GET /api/v1/meals
-export const getSavedMeals = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.user || !req.user.id) {
-      return next(new AppError("User not authenticated", 401));
-    }
-    const ownerId = req.user.id;
-
-    const result = await Meal.find({ ownerId });
-    res.status(200).json({ success: true, data: result });
+export const getSavedMeals = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user || !req.user.id) {
+    return next(new AppError("User not authenticated", 401));
   }
-);
+  const ownerId = req.user.id;
+
+  const result = await Meal.find({ ownerId });
+  res.status(200).json({ success: true, data: result });
+});
 
 // @desc    Add a new meal
 // @access  Private
 // @route   POST /api/v1/meals/addMeal
-export const addMeal = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { image, spoonacularId, title, sourceUrl } = req.body;
+export const addMeal = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { image, spoonacularId, title, sourceUrl } = req.body;
 
-    if (!req.user || !req.user.id) {
-      return next(new AppError("User not authenticated", 401));
-    }
-    const ownerId = req.user.id;
-    if (!image || !spoonacularId || !title || !sourceUrl) {
-      return next(new AppError("All fields are required", 400));
-    }
-
-    const meal = await Meal.create({
-      image,
-      spoonacularId,
-      title,
-      sourceUrl,
-      ownerId,
-    });
-
-    res.status(200).json({
-      status: "success",
-      data: meal,
-    });
+  if (!req.user || !req.user.id) {
+    return next(new AppError("User not authenticated", 401));
   }
-);
+  const ownerId = req.user.id;
+  if (!image || !spoonacularId || !title || !sourceUrl) {
+    return next(new AppError("All fields are required", 400));
+  }
+
+  const meal = await Meal.create({
+    image,
+    spoonacularId,
+    title,
+    sourceUrl,
+    ownerId,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: meal,
+  });
+});
 
 // @desc    Update a meal
 // @access  Private
 // @route   PUT /api/v1/meals/updateMeal/:id
-export const updateMeal = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
-    const { title } = req.body;
+export const updateMeal = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { title } = req.body;
 
-    if (!req.user || !req.user.id) {
-      return next(new AppError("User not authenticated", 401));
-    }
-    const ownerId = req.user.id;
-    if (!title) {
-      return next(new AppError("Please provide us with name!", 400));
-    }
-
-    const updatedMeal = await Meal.findOneAndUpdate(
-      { _id: id, ownerId },
-      { title },
-      { new: true, runValidators: true }
-    );
-
-    if (!updatedMeal) {
-      return next(
-        new AppError(
-          "Meal not found or you are not authorized to update it.",
-          404
-        )
-      );
-    }
-
-    res.status(200).json({
-      status: "success",
-      data: updatedMeal,
-    });
+  if (!req.user || !req.user.id) {
+    return next(new AppError("User not authenticated", 401));
   }
-);
+  const ownerId = req.user.id;
+  if (!title) {
+    return next(new AppError("Please provide us with name!", 400));
+  }
+
+  const updatedMeal = await Meal.findOneAndUpdate({ _id: id, ownerId }, { title }, { new: true, runValidators: true });
+
+  if (!updatedMeal) {
+    return next(new AppError("Meal not found or you are not authorized to update it.", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: updatedMeal,
+  });
+});
 // @desc    Delete a meal
 // @access  Private
 // @route   DELETE /api/v1/meals/:id
-export const deleteMeal = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
+export const deleteMeal = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
 
-    if (!req.user || !req.user.id) {
-      return next(new AppError("User not authenticated", 401));
-    }
-    const ownerId = req.user.id;
-
-    const deleteMeal = await Meal.findOneAndDelete({ _id: id, ownerId });
-
-    if (!deleteMeal) {
-      return next(
-        new AppError(
-          "Meal not found or you are not authorized to update it.",
-          404
-        )
-      );
-    }
-
-    res.status(200).json({
-      status: "success",
-      data: {},
-    });
+  if (!req.user || !req.user.id) {
+    return next(new AppError("User not authenticated", 401));
   }
-);
+  const ownerId = req.user.id;
+
+  const deleteMeal = await Meal.findOneAndDelete({ _id: id, ownerId });
+
+  if (!deleteMeal) {
+    return next(new AppError("Meal not found or you are not authorized to update it.", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {},
+  });
+});
