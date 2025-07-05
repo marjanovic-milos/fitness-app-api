0bc0f29cb7f8727f54a5492754c73e1313168c24
Milos Marjanovic <miskoo995@gmail.com>
2025-07-06 00:19:59 +0200

    finishing up scheduler routes

--- routes/schedulerRoutes.ts
+++ routes/schedulerRoutes.ts
@@ -1,19 +1,15 @@
-const express = require("express");
-const {
-  getAllEventsByTrainer,
-  createEvent,
-} = require("../controllers/schedulerController");
+import express from "express";
+const { getTrainerEvents, createEvent, getEvent, getClientEvents, updateEvent, deleteEvent } = require("../controllers/schedulerController");
 import { protect, authorize } from "../middleware/auth";
 
 const router = express.Router();
 
-router.get(
-  "/trainerSchedule",
-  protect,
-  authorize("admin"),
-  getAllEventsByTrainer
-);
+router.get("/trainerEvents", protect, authorize("admin"), getTrainerEvents);
+router.get("/clientEvents", protect, authorize("user"), getClientEvents);
 
-router.post("/", protect, authorize("admin"), createEvent);
+router.post("/createEvent", protect, authorize("admin"), createEvent);
+router.put("/:id", protect, authorize("admin"), updateEvent);
+router.delete("/:id", protect, authorize("admin"), deleteEvent);
+router.get("/:id", protect, getEvent);
 
 export default router;
