4c9159f634cd2fd46718fb75ab65957119bb90da
Milos <miskoo995@gmail.com>
2025-07-28 16:59:02 +0200

    console log removed

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -18,7 +18,7 @@ export const protect = catchAsync(async (req, res, next) => {
     token = req.headers.authorization.split(" ")[1];
   }
 
-  console.log(token, "token");
+ 
   if (!token) {
     return next(new AppError("You are not logged in! Please log in to get access.", 401));
   }
