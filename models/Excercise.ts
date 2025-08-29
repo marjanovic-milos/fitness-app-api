b0675a98d83ac907ea65c007e614fa234c8d9d73
Milos <miskoo995@gmail.com>
2025-08-29 11:18:58 +0200

    add support for youtube

--- models/Excercise.ts
+++ models/Excercise.ts
@@ -8,11 +8,14 @@ const ExcerciseSchema = new mongoose.Schema(
     },
     created: {
       type: Date,
-      default: Date.now(),
+      default: Date.now,
     },
-    video: {
+    sourceUrl: {
       type: String,
       required: true,
+    },
+    image: {
+      type: String,
     },
     notes: {
       type: String,
@@ -37,5 +40,22 @@ const ExcerciseSchema = new mongoose.Schema(
     },
   }
 );
+
+function getYoutubeId(url: String) {
+  const regExp =
+    /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[2].length === 11 ? match[2] : null;
+}
+
+ExcerciseSchema.pre("save", function (next) {
+  if (this.isModified("sourceUrl")) {
+    const videoId = getYoutubeId(this.sourceUrl);
+    if (videoId) {
+      this.image = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+    }
+  }
+  next();
+});
 
 export default mongoose.model("Excercise", ExcerciseSchema);
